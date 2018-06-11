8312ea82dbc1fabc1e934664545afab40a141edc
JFstandart <Juanfelipe.u.c7@gmail.com>
2018-06-11 17:40:41 -0500

    Setting Register page

---
+++ client/src/components/Register.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <h1>Register</h1>
+    <input type="text"
+           name="email"
+           v-model="email"
+           placeholder="Give me your email">
+    <br>
+    <input type="password"
+           name="password"
+           v-model="password"
+           placeholder="What is your pass?">
+    <br>
+    <button @click="register" type="button" name="button">Register</button>
+  </div>
+</template>
+
+<script>
+import authenticationService from '@/services/authenticationService'
+export default {
+  data () {
+    return {
+      email: '',
+      password:''
+    }
+  },
+  methods: {
+    async register () {
+      const response = await authenticationService.register({
+        email:this.email,
+        password:this.password
+      })
+      console.log(response.data);
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- server/src/app.js
+++ server/src/app.js
@@ -8,9 +8,9 @@ app.use(morgan('combined'))
 app.use(bodyParser.json())
 app.use(cors())
 
-app.get('/', (req, res) => {
+app.post('/register', (req, res) => {
   res.send({
-    message: 'hey'
+    message: `${req.body.email} was resgistered`
   })
 })
 
