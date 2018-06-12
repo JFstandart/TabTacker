278817e763bca13108e728d0c0bb990e1dcdbb69
JFstandart <Juanfelipe.u.c7@gmail.com>
2018-06-11 22:47:43 -0500

    Models, Joi and Vuetify on register page

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -1,41 +1,50 @@
 <template>
-  <div>
-    <h1>Register</h1>
-    <input type="text"
-           name="email"
-           v-model="email"
-           placeholder="Give me your email">
-    <br>
-    <input type="password"
-           name="password"
-           v-model="password"
-           placeholder="What is your pass?">
-    <br>
-    <button @click="register" type="button" name="button">Register</button>
-  </div>
+<v-layout column>
+  <v-flex xs6 offset-xs3>
+    <div class="white elevation-2">
+      <v-toolbar flat dense class="cyan" dark>
+        <v-toolbar-title>Register</v-toolbar-title>
+      </v-toolbar>
+      <div class="pl-4 pr-4 pt-2 pb-2">
+        <input type="text" name="email" v-model="email" placeholder="Give me your email">
+        <br>
+        <input type="password" name="password" v-model="password" placeholder="What is your pass?">
+        <br>
+        <div class="error" v-html="error" />
+        <v-btn class="cyan" @click="register" type="button" name="button">Register</v-btn>
+      </div>
+    </div>
+  </v-flex>
+</v-layout>
 </template>
 
 <script>
-import authenticationService from '@/services/authenticationService'
+import AuthenticationService from '@/services/authenticationService'
 export default {
   data () {
     return {
       email: '',
-      password:''
+      password: '',
+      error: null
     }
   },
   methods: {
     async register () {
-      const response = await authenticationService.register({
-        email:this.email,
-        password:this.password
-      })
-      console.log(response.data);
+      try {
+        await AuthenticationService.register({
+          email: this.email,
+          password: this.password
+        })
+      } catch (error) {
+        this.error = error.response.data.error
+      }
     }
   }
 }
 </script>
 
 <style scoped>
-
+.error {
+  color: red;
+}
 </style>
--- server/src/app.js
+++ server/src/app.js
@@ -1,17 +1,21 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const cors = require('cors')
-const morgan = require('morgan')
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const morgan = require('morgan');
+const {
+  sequelize
+} = require('./models');
+const config = require('./config/config');
 
-const app = express()
-app.use(morgan('combined'))
-app.use(bodyParser.json())
-app.use(cors())
+const app = express();
+app.use(morgan('combined'));
+app.use(bodyParser.json());
+app.use(cors());
 
-app.post('/register', (req, res) => {
-  res.send({
-    message: `${req.body.email} was resgistered`
-  })
-})
+require('./routes')(app);
 
-app.listen(process.env.PORT || 3000)
+sequelize.sync()
+  .then(() => {
+    app.listen(config.port);
+    console.log(`Running at ${config.port}`);
+  });
